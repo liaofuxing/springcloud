d10c13ebfe550246eb36d1509f6ae6b104b187e3
liaofuxing <597429743@qq.com>
2019-03-07 14:00:23 +0800

    第一次初始化

---
+++ system/src/main/java/com/myspringcloud/system/SystemApplication.java
@@ -0,0 +1,15 @@
+package com.myspringcloud.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+
+}
