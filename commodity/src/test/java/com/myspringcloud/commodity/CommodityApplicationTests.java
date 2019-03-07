269a9b04020820f7fec1c3fcddb26faeab55cc58
liaofuxing <597429743@qq.com>
2019-03-08 02:46:46 +0800

    测试类和配置文件提交

---
+++ commodity/src/test/java/com/myspringcloud/commodity/CommodityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myspringcloud.commodity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommodityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
