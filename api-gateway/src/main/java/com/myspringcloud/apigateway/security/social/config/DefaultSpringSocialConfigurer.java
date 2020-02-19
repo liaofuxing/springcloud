c1cb59efe21471f4a350edfda617b0940298f117
liaofuxing <liaofuxing@outlook.com>
2020-02-19 19:00:41 +0800

    解决冲突

---
+++ api-gateway/src/main/java/com/myspringcloud/apigateway/security/social/config/DefaultSpringSocialConfigurer.java
@@ -0,0 +1,35 @@
+package com.myspringcloud.apigateway.security.social.config;
+
+
+import org.springframework.social.security.SocialAuthenticationFilter;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+/**
+ * SpringSocialConfigurer配置类
+ * @author liaofuxing
+ * @date 2019/03/15 0:18
+ * @E-mail liaofuxing@outlook.com
+ */
+public class DefaultSpringSocialConfigurer extends SpringSocialConfigurer {
+
+    private String filterProcessesUrl;
+
+    public DefaultSpringSocialConfigurer(String filterProcessesUrl) {
+        this.filterProcessesUrl = filterProcessesUrl;
+    }
+
+    @Override
+    protected <T> T postProcess(T object) {
+        SocialAuthenticationFilter filter = (SocialAuthenticationFilter)super.postProcess(object);
+        filter.setFilterProcessesUrl(filterProcessesUrl);
+        return (T) filter;
+    }
+
+    public String getFilterProcessesUrl() {
+        return filterProcessesUrl;
+    }
+
+    public void setFilterProcessesUrl(String filterProcessesUrl) {
+        this.filterProcessesUrl = filterProcessesUrl;
+    }
+}
---
+++ api-gateway/src/main/java/com/myspringcloud/apigateway/security/social/config/SocialAutoConfigurerAdapter.java
@@ -0,0 +1,22 @@
+package com.myspringcloud.apigateway.security.social.config;
+
+import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.core.env.Environment;
+
+/**
+ * Spring boot 1.5.x SocialAutoConfigurerAdapter 源码，2.x被移除，自己重新从1.5.x中抽取
+ * @author liaofuxing
+ * @date 2019/03/15 0:18
+ * @E-mail liaofuxing@outlook.com
+ */
+public abstract class SocialAutoConfigurerAdapter extends SocialConfigurerAdapter {
+    public SocialAutoConfigurerAdapter() {
+
+    }
+    public void addConnectionFactories(ConnectionFactoryConfigurer configurer, Environment environment) {
+        configurer.addConnectionFactory(this.createConnectionFactory());
+    }
+    protected abstract ConnectionFactory<?> createConnectionFactory();
+}
