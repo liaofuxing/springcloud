5d09b2e6d34679d9922dbcb1566de07473f55e9c
liaofuxing <liaofuxing@outlook.com>
2020-02-20 23:28:44 +0800

    修改包路径

--- system/system-server/src/main/java/com/myspringcloud/SystemApplication.java
+++ system/system-server/src/main/java/com/myspringcloud/SystemApplication.java
@@ -1,11 +1,18 @@
-package system;
+package com.myspringcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+/**
+ * @author liaofuxing
+ * @date 2019/03/08 2:40
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class SystemApplication {
 
     public static void main(String[] args) {
