580083f85010a9524dd37fa854daa3772c6b521c
liaofuxing <liaofuxing@outlook.com>
2020-03-04 14:27:32 +0800

    分页调整

--- mall/src/main/java/com/myspringcloud/commodity/repository/CommodityRepository.java
+++ mall/src/main/java/com/myspringcloud/commodity/repository/CommodityRepository.java
@@ -1,7 +1,6 @@
 package com.myspringcloud.commodity.repository;
 
 import com.myspringcloud.commodity.entity.CommodityInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
