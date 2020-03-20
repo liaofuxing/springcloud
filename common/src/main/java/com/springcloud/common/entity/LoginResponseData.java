b5b9e0417de4071a1502778a253d51ee9cb789e6
无情的搬砖工 <597429743@qq.com>
2020-03-20 23:20:56 +0800

    将类迁移到公共项目

---
+++ common/src/main/java/com/springcloud/common/entity/LoginResponseData.java
@@ -0,0 +1,18 @@
+package com.springcloud.common.entity;
+
+import lombok.Data;
+
+@Data
+public class LoginResponseData {
+
+    private String username;
+    private String token;
+
+    public LoginResponseData() {
+
+    }
+    public LoginResponseData(String username, String token) {
+        this.username = username;
+        this.token = token;
+    }
+}
