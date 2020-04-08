966739ca2dbfd6b990f6f0541ece5bd1a49bcacd
无情的搬砖工 <597429743@qq.com>
2020-04-08 12:23:16 +0800

    新增java对象深度拷贝工具类

---
+++ common/src/main/java/com/springcloud/common/utils/callback/BeanCopyUtilCallBack.java
@@ -0,0 +1,12 @@
+package com.springcloud.common.utils.callback;
+
+@FunctionalInterface
+public interface BeanCopyUtilCallBack <S, T> {
+
+    /**
+     * 定义默认回调方法
+     * @param t
+     * @param s
+     */
+    void callBack(S t, T s);
+}
